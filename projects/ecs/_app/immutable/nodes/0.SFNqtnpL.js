cbde1570bb82ffa35bea1d8281548da95d7ceef5
Zach Winters <zach_winters@sweetwater.com>
2024-01-31 08:02:26 -0500

    Add ecs demo

---
+++ projects/ecs/_app/immutable/nodes/0.SFNqtnpL.js
@@ -0,0 +1,1 @@
+import{m as a,b as l,e as n,g as s}from"../chunks/disclose-version.h3ui14po.js";import{c,p as m}from"../chunks/runtime.FAD_NtMZ.js";const u=!0,i=Object.freeze(Object.defineProperty({__proto__:null,prerender:u},Symbol.toStringTag,{value:"Module"}));function _(e,o){m(o,!1);var r=s(e),t=n(r);a(t,o.children,{},null),l(e,r),c()}export{_ as component,i as universal};
